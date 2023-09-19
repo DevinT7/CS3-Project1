0466b40110bb1fa888095e5d9a5738fe40ee691b
DevinT7 <136363837+DevinT7@users.noreply.github.com>
2023-09-19 13:01:41 -0500

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -8,12 +8,14 @@ public class MyWorld extends World {
     private Jack jack;
     private Ninja ninja;
     private Block block;
+    private String tiles[][]; 
     public MyWorld() 
     {
-        setBackground("1241668-blank-mario-world.jpg");
+        setBackground("img/BG/BG.png");
         Mayflower.showBounds(true);
         cat = new Cat();
         addObject(cat, 400, 0);
+        tiles = new String[6][8];
         //dog = new Dog();
         //addObject(dog, 200, 100);
         //jack = new Jack();
@@ -21,12 +23,49 @@ public class MyWorld extends World {
         //ninja = new Ninja();
         //addObject(ninja, 100, 400);
         
-        block = new Block();
-        addObject(block, 400,100);
-        Block block1 = new Block();
-        addObject(block1, 528, 372);
+        //block = new Block();
+        //addObject(block, 400,100);
+        //Block block1 = new Block();
+        //addObject(block1, 528, 372);
+        buildWorld(); 
         
         
+    }
+    
+    public void buildWorld(){
+        for (int i = 0; i < tiles.length; i++){
+            tiles[i] = new String[tiles[0].length]; 
+            for (int x = 0; x < tiles[0].length; x++){
+                tiles[i][x] = "";
+            }
+        }
+        
+        
+        for (int x = 0; x < tiles[0].length; x++){
+                tiles[5][x] = "ground"; 
+            }
+            
+        
+        for (int r = 0; r < tiles.length; r++){
+            for (int c = 0; c < tiles[r].length; c++){
+                if (tiles[r][c].equals("ground")){
+                    addObject(new Block(), c * 128, r + 600 - 120);
+                }
+            }
+        }
+    }
+    
+    public void addRandomObjects(){
+        for (int r = 0; r < tiles.length; r++){
+            for (int c = 0; c < tiles[r].length; c++){
+                int rand = (int)(Math.random() * tiles.length);
+                
+                if ((rand < 3) && !(tiles[r][c].equals("ground"))){
+                   addObject(new Yarn(), (c*100), (r*100)); 
+                   tiles[r][c] = "yarn"; 
+                }
+            }
+        }
     }
     
     public void act()
